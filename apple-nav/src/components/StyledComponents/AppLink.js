bdee18207daaf8b9c964dfe0f922665b441b42e8
erozonachi <erozonachi.james@gmail.com>
2019-06-11 17:54:14 +0100

    Add more styles to app link styled component

--- apple-nav/src/components/StyledComponents/AppLink.js
+++ apple-nav/src/components/StyledComponents/AppLink.js
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 import { NavLink, } from 'react-router-dom';
 
 const AppLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   color: inherit;
   text-decoration: none;
   &:link, &:visited {
     color:inherit;
+  }
+
+  img {
+    max-height: 75px;
   }
 `;
 
