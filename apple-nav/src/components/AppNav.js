bb20a67cbc786484cf5382ab2c84b72435e20c06
erozonachi <erozonachi.james@gmail.com>
2019-06-11 16:35:08 +0100

    Make changes to app nav component

--- apple-nav/src/components/AppNav.js
+++ apple-nav/src/components/AppNav.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect, } from 'react';
+import {BrowserRouter as Router, Route, } from 'react-router-dom';
 import Nav from './StyledComponents/Nav';
+import Link from './Link';
+import NavWrapper from './StyledComponents/NavWrapper';
 
 export default function AppNav(props) {
 
   const [links, setLinks] = useState(props.links);
 
+  return (
+    <NavWrapper>
+      <Router>
+        <Nav>
+          {links.map(link => <Link link={link} />)}
+        </Nav>
+        <Route>
+          <NavWrapper>
+  {links.map(link => link.subLinks? <Nav>{link.subLinks.map(item => {
+    item.id = `${link.id}/${item.id}`;
+    return <Link link={item} />;
+  })}</Nav> : null)}
+          </NavWrapper>
+        </Route>
+      </Router>
+    </NavWrapper>
+  );
 }
