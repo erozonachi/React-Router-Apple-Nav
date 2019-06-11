515a8be9e276f03744bad2b33bd36b5f24dc30e1
erozonachi <erozonachi.james@gmail.com>
2019-06-11 14:27:12 +0100

    Create nav styled components

---
+++ apple-nav/src/components/StyledComponents/Nav.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: space-evenly;
+  width: 100%;
+  padding: 10px 0;
+`;
+
+export default Nav;
