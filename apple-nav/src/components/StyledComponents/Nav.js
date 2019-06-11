bf5da0dffc4808e48de792c8cc9d926f5ea8bc05
erozonachi <erozonachi.james@gmail.com>
2019-06-11 17:43:58 +0100

    Add more styles to nav styled component

--- apple-nav/src/components/StyledComponents/Nav.js
+++ apple-nav/src/components/StyledComponents/Nav.js
@@ -3,9 +3,15 @@ import styled from 'styled-components';
 const Nav = styled.nav`
   display: flex;
   justify-content: space-evenly;
-  align-items: space-evenly;
+  align-items: baseline;
   width: 100%;
   padding: 10px 0;
+  background-color: #333;
+  color: #fff;
+  &:last-of-type {
+    background-color: #eee;
+    color: #111;
+  }
 `;
 
 export default Nav;
