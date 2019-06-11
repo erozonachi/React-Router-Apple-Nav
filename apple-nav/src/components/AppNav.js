ee3bcaaf632a1e9a03312e40eaabba92cc2d4960
erozonachi <erozonachi.james@gmail.com>
2019-06-11 15:25:22 +0100

    Create app nav component

---
+++ apple-nav/src/components/AppNav.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, } from 'react';
+import Nav from './StyledComponents/Nav';
+
+export default function AppNav(props) {
+
+  const [links, setLinks] = useState(props.links);
+
+}
