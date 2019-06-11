2f5015cb5587280c15a169f277899ce965744f0b
erozonachi <erozonachi.james@gmail.com>
2019-06-11 14:30:18 +0100

    Create nav wrapper styled component

---
+++ apple-nav/src/components/StyledComponents/NavWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const NavWrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: space-evenly;
+  width: 100%;
+  padding: 0;
+`;
+
+export default NavWrapper;
