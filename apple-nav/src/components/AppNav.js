7aa651c5a95c303b7105354a4fc1102a1439472f
erozonachi <erozonachi.james@gmail.com>
2019-06-11 18:36:16 +0100

    Pass active prop to the currently focused link

--- apple-nav/src/components/AppNav.js
+++ apple-nav/src/components/AppNav.js
@@ -5,18 +5,23 @@ import Link from './Link';
 import NavWrapper from './StyledComponents/NavWrapper';
 
 export default function AppNav(props) {
-
+  const [active, setActive] = useState('');
+  useEffect(() => {
+    setActive(document.location.pathname);
+  });
   return (
     <NavWrapper>
       <Router>
         <Nav>
-          {props.navLinks.map(link => <Link key={link.id} link={link} />)}
+          {props.navLinks.map(link => 
+          <Link key={link.id} link={link} active={active.includes(link.id)? true : false} />)}
         </Nav>
-          <NavWrapper>
-  {props.navLinks.map(link => link.subLinks? <Route key={link.id} path={`/${link.id}`} render={(props) => (
-    <Nav darken={link.id === 'tv'? true : false}>{link.subLinks.map(item => <Link key={item.id} link={item} {...props} />)}</Nav>
-  )} /> : null)}
-          </NavWrapper>
+        <NavWrapper>
+          {props.navLinks.map(link => link.subLinks? 
+          <Route key={link.id} path={`/${link.id}`} render={(props) => (
+            <Nav darken={link.id === 'tv'? true : false}>{link.subLinks.map(item => <Link key={item.id} link={item} {...props} />)}</Nav>
+          )} /> : null)}
+        </NavWrapper>
       </Router>
     </NavWrapper>
   );
